dbb3bd511fef24fa2f89c977c171a0c27b73a596
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-10-03 17:08:44 +0400

    add template for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  let result = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string' && members[i][0] !== ' ') {
+      result.push(members[i][0]);
+    }
+  };
+  if (result === []) {
+    return false
+  };
+  result = result.sort().join('');
+
+  return result.toUpperCase();
 };
